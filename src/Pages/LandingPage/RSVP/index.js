6434fdf9045a856b53f649d4df64a7d15ca6dc98
wolenskyatwork <elizabeth@fubo.tv>
2018-09-03 21:33:28 -0400

    trying

--- src/Pages/LandingPage/RSVP/index.js
+++ src/Pages/LandingPage/RSVP/index.js
@@ -6,9 +6,11 @@ class RSVP extends Component {
     constructor() {
         super()
         this.state = {
-            name: '',
+            finished: false,
+            first_name: '',
+            last_name: '',
             attending: true,
-            foodChoice: 'steak',
+            foodChoice: 2,
             diet_stuff: '',
         };
     }
@@ -45,23 +47,23 @@ class RSVP extends Component {
                     </div>
                     <div className='food_choices'>
                         <div
-                            onClick={() => this.setState({ foodChoice: 'steak' }) }
+                            onClick={() => this.setState({ foodChoice: 2 }) }
                             className='food_choice'>
-                            <img className={`circle_selector ${foodChoice === 'steak' ? 'show' : 'hide' }`} src={require('../../../images/scribble-circle-3.png')}/>
+                            <img className={`circle_selector ${foodChoice === 2 ? 'show' : 'hide' }`} src={require('../../../images/scribble-circle-3.png')}/>
                             <img className='cow_selector' src={require('../../../images/cow.svg')}/>
                             Steak
                         </div>
                         <div
-                            onClick={() => this.setState({ foodChoice: 'chicken' }) }
+                            onClick={() => this.setState({ foodChoice: 1 }) }
                             className='food_choice'>
-                            <img className={`circle_selector ${foodChoice === 'chicken' ? 'show' : 'hide' }`} src={require('../../../images/scribble-circle-3.png')}/>
+                            <img className={`circle_selector ${foodChoice === 1 ? 'show' : 'hide' }`} src={require('../../../images/scribble-circle-3.png')}/>
                             <img className='chicken_selector' src={require('../../../images/line-drawing-chicken-31.png')}/>
                             Chicken
                         </div>
                         <div
-                            onClick={() => this.setState({ foodChoice: 'veggie' }) }
+                            onClick={() => this.setState({ foodChoice: 3 }) }
                             className='food_choice'>
-                            <img className={`circle_selector ${foodChoice === 'veggie' ? 'show' : 'hide' }`} src={require('../../../images/scribble-circle-3.png')}/>
+                            <img className={`circle_selector ${foodChoice === 3 ? 'show' : 'hide' }`} src={require('../../../images/scribble-circle-3.png')}/>
                             <img className='chicken_selector' src={require('../../../images/root.png')}/>
                             Veggie
                         </div>
@@ -86,7 +88,39 @@ class RSVP extends Component {
     }
 
     sendRSVP = () => {
-        console.log(this.state)
+        console.log(JSON.stringify({
+            first_name: this.state.first_name,
+            last_name: this.state.last_name,
+            food_option_id: 1, // this.state.foodChoice,
+            attending: this.state.attending,
+            dietary_note: this.state.diet_stuff,
+        }))
+
+        // this.setState({
+        //     finished: true,
+        // })
+
+        // var myHeaders = new Headers();
+        //
+        // myHeaders.append('Content-Type', 'text/xml');
+        // myHeaders.get('Content-Type') // should return 'text/xml'
+
+        const myHeaders = new Headers({
+            'Content-Type': 'application/json',
+        })
+
+        fetch('https://lizzieandcarlos.com/rsvps', {
+            method: 'POST',
+            // mode: 'no-cors',
+            headers: myHeaders,
+            body: JSON.stringify({ "rsvp": {
+                first_name: this.state.first_name,
+                last_name: this.state.last_name,
+                food_option_id: this.state.foodChoice,
+                attending: this.state.attending,
+                dietary_note: this.state.diet_stuff,
+            }})
+        }).then(this.setState({ finished: true }))
     }
 
     render() {
@@ -96,39 +130,54 @@ class RSVP extends Component {
             <div id='rsvp_here' className='RSVP'>
                 <div className={'rsvp_left'}></div>
                 <div className={'rsvp_right'}>
-                    <div className={'rsvp_header lazy_spacing'}>Hey there! We hope you can make it!</div>
-                        <div className={'name_box'}>
-                            <div className={'name_box_title'}>Your Name:</div>
+                    { !this.state.finished && (
+                        <div>
+                            <div className={'rsvp_header lazy_spacing'}>Hey there! We hope you can make it!</div>
+                            <div className={'name_box'}>
+                                <div className={'name_box_title'}>First Name:</div>
+                                    <input
+                                        className={'input_box name_box_input'}
+                                        onChange={this.handleInputChange}
+                                        type='text'
+                                        name='first_name'
+                                    />
+                            </div>
+                            <div className={'name_box'}>
+                                <div className={'name_box_title'}>Last Name:</div>
                                 <input
                                     className={'input_box name_box_input'}
                                     onChange={this.handleInputChange}
                                     type='text'
-                                    name='name'
+                                    name='last_name'
                                 />
-                        </div>
-                        <div className={'attending_box lazy_spacing'}>
-                            <div className={'center_vertical choice_title'}>Attending:</div>
-                            <div className={'attending_options'}>
-                                <div
-                                    onClick={() => this.setState({ attending: true })}
-                                    className={'center_vertical attending_option'}>
-                                    <img className={`circle_selector ${attending ? 'show' : 'hide' }`} src={require('../../../images/scribble-circle-3.png')}/>
-                                    <img className='chicken_selector' src={require('../../../images/line-drawing-chicken-31.png')}/>
-                                    <div>Yes, I'll be there! :D</div>
-                                </div>
-                                <div
-                                    onClick={() => this.setState({ attending: false })}
-                                    className={'center_vertical attending_option'}>
-                                    <img className={`circle_selector ${attending ? 'hide' : 'show' }`} src={require('../../../images/scribble-circle-3.png')}/>
-                                    <img className='chicken_selector' src={require('../../../images/sad.png')}/>
-                                    <div>Sorry, I can't make it :(</div>
+                            </div>
+                            <div className={'attending_box lazy_spacing'}>
+                                <div className={'center_vertical choice_title'}>Attending:</div>
+                                <div className={'attending_options'}>
+                                    <div
+                                        onClick={() => this.setState({ attending: true })}
+                                        className={'center_vertical attending_option'}>
+                                        <img className={`circle_selector ${attending ? 'show' : 'hide' }`} src={require('../../../images/scribble-circle-3.png')}/>
+                                        <img className='chicken_selector' src={require('../../../images/line-drawing-chicken-31.png')}/>
+                                        <div>Yes, I'll be there! :D</div>
+                                    </div>
+                                    <div
+                                        onClick={() => this.setState({ attending: false })}
+                                        className={'center_vertical attending_option'}>
+                                        <img className={`circle_selector ${attending ? 'hide' : 'show' }`} src={require('../../../images/scribble-circle-3.png')}/>
+                                        <img className='chicken_selector' src={require('../../../images/sad.png')}/>
+                                        <div>Sorry, I can't make it :(</div>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div className={'bottom_rsvp'}>
-                            { this.state.attending ? this.attending() : this.notAttending() }
-                        </div>
-                        <div className={'rsvp_button'} onClick={this.sendRSVP}>RSVP!</div>
+                            <div className={'bottom_rsvp'}>
+                                { this.state.attending ? this.attending() : this.notAttending() }
+                            </div>
+                            <div className={'rsvp_button'} onClick={this.sendRSVP}>RSVP!</div>
+                         </div>
+                    )}
+                    { this.state.finished && this.state.attending && (<div>Thanks! See you in October!</div>) }
+                    { this.state.finished && !this.state.attending && (<div>Thanks! Sorry you can't make it :(</div>) }
                 </div>
             </div>
         );
